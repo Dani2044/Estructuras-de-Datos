8a67c7ec291b4aa0448c4ccf73f5077973412c38
Daniel Felipe Felipe <24851532-Daniel-Felip204@users.noreply.replit.com>
2024-07-29 21:51:00 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "menu.h"
+
+int main() {
+    Menu menu;
+    menu.manejarEntradaUsuario(); // Inicia el menú y maneja la entrada del usuario
+    return 0;
+}
---
+++ menu.hxx
@@ -0,0 +1,10 @@
+#ifndef __MENU__HXX__
+#define __MENU__HXX__
+
+// -------------------------------------------------------------------------
+// Aquí va la implementación de todas las funciones en menu.h especificando que falta la implementación de todos las que no son de la interfaz gráfica
+// -------------------------------------------------------------------------
+
+#endif // __MENU__HXX__
+
+// eof - menu.hxx
