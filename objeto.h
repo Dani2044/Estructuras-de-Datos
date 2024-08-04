4ec012dfd321206427fa8eb9549a683cfe69ddd0
Daniel Castro <danielfcm2004@gmail.com>
2024-08-03 22:57:54 -0500

    Update objeto.h

--- objeto.h
+++ objeto.h
@@ -6,10 +6,9 @@
 #include <string>
 #include <sstream>
 
-// No se realiza objeto.hxx porque la implementación es muy sencilla
 class Objeto {
 private:
-    std::string nombre_objeto;
+    std::string mesh_name;
     int n_vertices;
     std::vector<int> x;
     std::vector<int> y;
@@ -19,12 +18,12 @@ private:
 
 public:
     // Constructor
-    Objeto(std::string nombre_objeto, int n_vertices, std::vector<int> x, std::vector<int> y, std::vector<int> z,
+    Objeto(std::string mesh_name, int n_vertices, std::vector<int> x, std::vector<int> y, std::vector<int> z,
            std::vector<int> cj, std::vector<int> ij)
-        : nombre_objeto(nombre_objeto), n_vertices(n_vertices), x(x), y(y), z(z), cj(cj), ij(ij) {}
+        : mesh_name(mesh_name), n_vertices(n_vertices), x(x), y(y), z(z), cj(cj), ij(ij) {}
 
     // Getters
-    std::string getNombreObjeto() const { return nombre_objeto; }
+    std::string getMeshName() const { return mesh_name; }
     int getNVertices() const { return n_vertices; }
     std::vector<int> getX() const { return x; }
     std::vector<int> getY() const { return y; }
@@ -33,7 +32,7 @@ public:
     std::vector<int> getIj() const { return ij; }
 
     // Setters
-    void setNombreObjeto(const std::string& nombre_objeto) { this->nombre_objeto = nombre_objeto; }
+    void setMeshName(const std::string& mesh_name) { this->mesh_name = mesh_name; }
     void setNVertices(int n_vertices) { this->n_vertices = n_vertices; }
     void setX(const std::vector<int>& x) { this->x = x; }
     void setY(const std::vector<int>& y) { this->y = y; }
@@ -41,16 +40,14 @@ public:
     void setCj(const std::vector<int>& cj) { this->cj = cj; }
     void setIj(const std::vector<int>& ij) { this->ij = ij; }
 
-    // ToString
     std::string toString() const {
         std::ostringstream oss;
         int totalAristas = 0;
-        
         for (const auto& num : cj) {
             totalAristas += num;
         }
     
-        oss << nombre_objeto << " contiene " << n_vertices << " vértices, ";
+        oss << mesh_name << " contiene " << n_vertices << " vértices, ";
         oss << totalAristas << " aristas y " << cj.size() << " caras.";
         return oss.str();
     }
